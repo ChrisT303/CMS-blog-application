985b9fd6a8fda5ce718b62a07f572be3c0929c7d
deftonechris <deftonechris@msn.com>
2022-08-30 15:04:55 -0600

    Edit is rendering, button errors, styling issues

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -5,18 +5,15 @@ const withAuth = require("../utils/auth");
 
 router.get("/", withAuth, async (req, res) => {
   try {
-    
     const postData = await Post.findAll({
-      where: { "user_id": req.session.user_id },
+      where: { user_id: req.session.user_id },
       include: [User],
     });
 
-    
     const posts = postData.map((post) => post.get({ plain: true }));
 
-    
     res.render("all-posts", {
-      layout: 'dashboard',
+      layout: "dashboard",
       posts,
     });
   } catch (err) {
@@ -24,25 +21,18 @@ router.get("/", withAuth, async (req, res) => {
   }
 });
 
-router.get('/edit/:id', withAuth, async (req, res) => {
-    try{
-        const editPost = await Post.findByPk(req.params.id);
-        const editData = editPost.map((post) => post.get({plain: true}));
-        console.log(editData)
-        res.render("edit", {
-          layout: dashboard,
-          editData,
-        });
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
+router.get("/edit/:id", withAuth, async (req, res) => {
+  const editPost = await Post.findByPk(req.params.id);
+  // const editData = editPost.map((post) => post.get({plain: true}));
+  console.log("editPost: ", editPost);
+  res.render("edit-post", { layout: "dashboard" });
+});
 
-router.get('/create', withAuth, (req, res) => {
-    res.render('create-post', {
-      layout: "dashboard",
-      // logged_in: true,
-    })
-})
+router.get("/create", withAuth, (req, res) => {
+  res.render("create-post", {
+    layout: "dashboard",
+    // logged_in: true,
+  });
+});
 
 module.exports = router;
